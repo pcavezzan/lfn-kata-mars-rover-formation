4dbd6aa639556917ecca9eec245c4d3773d04dbb
mathildeSL <mathilde.salthun-lassalle@lafoncierenumerique.com>
2022-10-13 15:01:49 +0200

    refacto tests, introduce parametrized test

--- src/test/kotlin/com/marsrover/domain/RoverTest.kt
+++ src/test/kotlin/com/marsrover/domain/RoverTest.kt
@@ -2,6 +2,8 @@ package com.marsrover.domain
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 internal class RoverTest {
 
@@ -17,35 +19,22 @@ internal class RoverTest {
         assertThat(rover).isEqualTo(expectedRover)
     }
 
-    @Test
-    internal fun `should move rover forward direction north`() {
+    @ParameterizedTest
+    @CsvSource("N,0,1", "S,0,-1")
+    internal fun `should move rover forward to given direction`(givenDirection: String, expectedX: Int, expectedY: Int) {
         val x = 0
         val y = 0
-        val direction = Direction.N
+        val direction = Direction.valueOf(givenDirection)
         val commands = listOf("f")
 
         val rover = Rover(Point(x, y), direction)
 
         rover.executeCommands(commands)
 
-        val expectedRover = Rover(Point(0, 1), direction)
+        val expectedRover = Rover(Point(expectedX, expectedY), direction)
         assertThat(rover).isEqualTo(expectedRover)
     }
 
-    @Test
-    internal fun `should move rover forward direction south`() {
-        val x = 0
-        val y = 0
-        val direction = Direction.S
-        val commands = listOf("f")
-
-        val rover = Rover(Point(x, y), direction)
-
-        rover.executeCommands(commands)
-
-        val expectedRover = Rover(Point(0, -1), direction)
-        assertThat(rover).isEqualTo(expectedRover)
-    }
 
 
 }
