f32fdedf3850e9d0b55a9b536d79abf9eb3f20eb
Yannick <yannick.virgili@labeo.fr>
2022-10-27 14:49:13 +0200

    Wip test integration

---
+++ src/main/kotlin/com/marsrover/RoverApplication.kt
@@ -0,0 +1,7 @@
+package com.marsrover
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class RoverApplication {
+}
--- src/test/kotlin/com/marsrover/infrastructure/api/RoverControllerIntegrationTest.kt
+++ src/test/kotlin/com/marsrover/infrastructure/api/RoverControllerIntegrationTest.kt
@@ -1,9 +1,15 @@
 package com.marsrover.infrastructure.api
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MockMvcBuilder
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
 @SpringBootTest
@@ -14,9 +20,23 @@ internal class RoverControllerIntegrationTest {
 
     private lateinit var mockMvc : MockMvc
 
+    @BeforeEach
+    internal fun setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webContext).build()
+    }
 
     @Test
     internal fun `should post command to rover`() {
-        TODO("Not yet implemented")
+        mockMvc.perform(MockMvcRequestBuilders.post("/commands")
+            .contentType(MediaType.APPLICATION_JSON).content("""
+                ['f']
+            """.trimIndent())).andExpect(MockMvcResultMatchers.status().isOk)
+            .andExpect(MockMvcResultMatchers.content().json("""
+                {
+                    x: 0,
+                    y: 1,
+                    direction: E
+                }
+            """.trimIndent()))
     }
 }
