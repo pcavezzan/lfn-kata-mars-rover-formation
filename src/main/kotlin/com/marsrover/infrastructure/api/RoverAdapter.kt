f3f743dd9b0aef0fba04a7a5b2e335b14620abe2
Patrice CAVEZZAN <pcavezzan@gmail.com>
2022-12-01 14:32:50 +0100

    Convert to expression body.

--- src/main/kotlin/com/marsrover/infrastructure/api/RoverAdapter.kt
+++ src/main/kotlin/com/marsrover/infrastructure/api/RoverAdapter.kt
@@ -3,7 +3,6 @@ package com.marsrover.infrastructure.api
 import com.marsrover.domain.application.MoveRoverForward
 
 class RoverAdapter(val moveRoverForward: MoveRoverForward) {
-    fun moveRoverForward(command: Array<String>): RoverPositionResponse {
-        return RoverPositionResponse.from(moveRoverForward.execute())
-    }
+    fun moveRoverForward(command: Array<String>): RoverPositionResponse =
+        RoverPositionResponse.from(moveRoverForward.execute())
 }
